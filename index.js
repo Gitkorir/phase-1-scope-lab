92ad2657b0c3021429900333855c1c1a9e271ee0
Arnold Korir <arnoldkorir201@gmail.com>
2025-03-10 13:16:41 +0300

    initial js structure

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+//declare a variable
+var customerName = "bob";
+// functional scope
+function firstName() {
+  return window.customerName;
+}
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+// Function to set the bestCustomer
+function setBestCustomer() {
+  window.bestCustomer = "not bob";
+}
+
+// Function to overwrite the bestCustomer
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer;
+}
+const leastFavoriteCustomer = "John";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "bob";
+}
